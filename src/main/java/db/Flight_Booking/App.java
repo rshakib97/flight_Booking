84041d9dc7beacbee10d947abfe364320e787997
rayhans1210@gmail.com <rayhans1210@gmail.com>
2021-01-31 21:48:00 -0500

    push 1.1

---
+++ src/main/java/db/Flight_Booking/App.java
@@ -0,0 +1,13 @@
+package db.Flight_Booking;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
